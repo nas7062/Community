f844a5e3d39a04ca9f47b66aa13d92dce7f5734b
nas7062 <nas7062@naver.com>
2025-12-11 21:22:02 +0900

    feat : searchinput 및 로고 생성

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,11 +1,19 @@
 import FeedList from "@/components/FeedList";
+import SearchInput from "@/components/SearchInput";
 import { colors } from "@/constants";
 import { useAuth } from "@/context/AuthContext";
 import { UserProfile } from "@/types";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { router } from "expo-router";
 import { useEffect, useRef } from "react";
-import { Pressable, StyleSheet } from "react-native";
+import {
+  Image,
+  Platform,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 
@@ -29,6 +37,18 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={styles.logo}
+        />
+        <SearchInput
+          readOnly
+          placeholder="글 제목 검색"
+          onPress={() => router.push("/posts/search")}
+        />
+      </View>
+
       <FeedList />
       {user && (
         <Pressable
@@ -60,5 +80,18 @@ const styles = StyleSheet.create({
     shadowColor: colors.BLACK,
     shadowOffset: { width: 1, height: 2 },
     elevation: 2,
+  },
+  inputContainer: {
+    marginBottom: 10,
+    paddingHorizontal: 16,
+    gap: 8,
+    backgroundColor: colors.WHITE,
+    flexDirection: "row",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  logo: {
+    height: 44,
+    width: 44,
+    borderRadius: 50,
   },
 });
